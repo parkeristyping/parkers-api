d200081f0c6b228afa0dab7c4d691b13de396263
Parker Lawrence <parker.alford@gmail.com>
2017-03-22 18:18:39 -0500

    + determine-delimiter & parse-delimited-string fns

--- src/parkers_api/core.clj
+++ src/parkers_api/core.clj
@@ -1,6 +1,25 @@
-(ns parkers-api.core)
+(ns parkers-api.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn determine-delimiter
+  "Scans a string for a ' ' (space), '|' (pipe), or ',' (comma) and returns
+   the first of those that it finds. Throws AssertionError if none found."
+  [s]
+  (let [delimiter (re-find #"[ |,]" s)]
+    (assert (some #{delimiter} [" ", "|", ","])
+            "No delimiter was found!")
+    delimiter))
+
+(defn parse-delimited-string
+  "Given a delimiter, parses a string into a vector of vectors. E.g.
+
+   one|two|three
+   four|five|six
+
+   [['one', 'two', 'three'], ['four', 'five', 'six']]"
+  [s delimiter]
+  (mapv
+   ;; Below we escape delimiter before converting to regex to avoid
+   ;; conflicts with regex special chars
+   #(str/split % (re-pattern (str "\\" delimiter)))
+   (str/split s #"\n")))
--- test/parkers_api/core_test.clj
+++ test/parkers_api/core_test.clj
@@ -2,6 +2,23 @@
   (:require [clojure.test :refer :all]
             [parkers-api.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-determine-delimiter
+  (testing "works for pipe"
+    (is (= "|" (determine-delimiter "1and234|76483"))))
+  (testing "works for comma"
+    (is (= "," (determine-delimiter ",\njudybloom"))))
+  (testing "works for space"
+    (is (= " " (determine-delimiter "hello@#$@#! what is up"))))
+  (testing "throws assertion error if no delimiter found"
+    (is (thrown? AssertionError (determine-delimiter "no\ndelimiter\nhere")))))
+
+(deftest test-parse-delimited-string
+  (testing "works for pipes"
+    (is (= [["one" "two" "three"]["four" "five" "six"]]
+           (parse-delimited-string "one|two|three\nfour|five|six" "|"))))
+  (testing "works for commas"
+    (is (= [["HEY" "___" "!@#!@"]]
+           (parse-delimited-string "HEY,___,!@#!@" ","))))
+  (testing "works for spaces"
+    (is (= [["x" "x"]["x"]["x"]]
+           (parse-delimited-string "x x\nx\nx" "|")))))
