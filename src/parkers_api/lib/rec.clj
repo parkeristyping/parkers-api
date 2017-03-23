a4f868652cf62eabf2f9394bc2dd80e48d67f96c
Parker Lawrence <parker.alford@gmail.com>
2017-03-23 11:27:18 -0500

    add some tests for full extract fn

--- src/parkers_api/lib/rec.clj
+++ src/parkers_api/lib/rec.clj
@@ -2,7 +2,8 @@
   (:require [clojure.pprint :as pprint]
             [clj-time.core :as t]
             [clj-time.format :as tf]
-            [parkers-api.lib.utils :as u]))
+            [parkers-api.lib.utils :as u]
+            [clojure.string :as str]))
 
 (def desc
   "Descending comparator"
@@ -28,11 +29,13 @@
   (tf/formatter "M/d/YYYY"))
 
 (defn clojurify
-  "Does needed type conversions when reading values from string"
+  "Does sanitization and type conversions when reading values from string"
   [record]
   (u/apply-conversion-map
    record
-   {:birth-date #(tf/parse time-format %)}))
+   {:birth-date #(tf/parse time-format %)
+    :gender str/lower-case
+    :favorite-color str/lower-case}))
 
 (defn printify
   "Converts record values to be more easily human-readable, where needed"
@@ -50,9 +53,17 @@
     records)))
 
 (defn extract-records-from-string
+  "Converts a string to a vector of records, given the following:
+   - Each line represents a new record
+   - Each line is delimited by a ',' '|' or ' ' to separate record fields
+   - None of those delimiters appear in the string other than as delimiters
+   - The order of fields is: last-name, first-name, gender, favorite-color, birth-date
+   - Birth-date is formatted 'M/d/YYYY' (in Joda Time terms:
+     http://www.joda.org/joda-time/key_format.html)
+   - No lines contain blank fields"
   [records-string]
   (let [delimiter (u/determine-delimiter records-string)
         records-vec (u/parse-delimited-string records-string delimiter)]
-    (-> records-vec
-        (u/vec->map [:last-name :first-name :gender :favorite-color :birth-date])
-        clojurify)))
+    (->> records-vec
+        (mapv #(u/vec->map % [:last-name :first-name :gender :favorite-color :birth-date]))
+        (mapv clojurify))))
--- test/parkers_api/lib/rec_test.clj
+++ test/parkers_api/lib/rec_test.clj
@@ -1,7 +1,35 @@
 (ns parkers-api.lib.rec-test
   (:require [clojure.test :refer :all]
             [parkers-api.lib.rec :refer :all]
-            [clj-time.core :as t]))
+            [clj-time.core :as t]
+            [clojure.string :as str]
+            [parkers-api.lib.utils :as u]
+            [parkers-api.lib.rec :as rec]))
+
+(def test-input-comma
+"Lawrence,Parker,male,blue,4/30/1990
+Brooks,Sofie,female,green,1/10/1990
+Shields,Brooke,female,yellow,4/30/1994
+Hardesty,Ben,male,blue,4/3/2007
+Glass,Philip,male,blue,2/9/1987")
+
+(def test-input-pipe
+  (str/replace test-input-comma #"," "|"))
+
+(def test-input-space
+  (str/replace test-input-comma #"," " "))
+
+(def test-records
+  [{:last-name "Lawrence" :first-name "Parker" :gender "male"
+    :favorite-color "blue" :birth-date (t/date-time 1990 4 30)}
+   {:last-name "Brooks" :first-name "Sofie" :gender "female"
+    :favorite-color "green" :birth-date (t/date-time 1990 1 10)}
+   {:last-name "Shields" :first-name "Brooke" :gender "female"
+    :favorite-color "yellow" :birth-date (t/date-time 1994 4 30)}
+   {:last-name "Hardesty" :first-name "Ben" :gender "male"
+    :favorite-color "blue" :birth-date (t/date-time 2007 4 3)}
+   {:last-name "Glass" :first-name "Philip" :gender "male"
+    :favorite-color "blue" :birth-date (t/date-time 1987 2 9)}])
 
 (deftest test-sort-by-gender
   (testing "sorts by gender (female, then male) then
@@ -49,9 +77,23 @@
 (deftest test-clojurify
   (testing "converts birth-date to joda time"
     (is (= {:birth-date (t/date-time 1990 4 30)}
-         (clojurify {:birth-date "4/30/1990"})))))
+           (clojurify {:birth-date "4/30/1990"}))))
+  (testing "lower-cases gender and favorite color to standardize"
+    (is (= {:gender "male" :favorite-color "red"}
+           (clojurify {:gender "Male" :favorite-color "RED"})))))
 
 (deftest test-printify
   (testing "converts birth-date to M/D/YYYY string"
     (is (= {:birth-date "4/30/1990"}
            (printify {:birth-date (t/date-time 1990 4 30)})))))
+
+(deftest test-extract-records-from-string
+  (testing "comma delimited"
+    (is (= test-records
+           (extract-records-from-string test-input-comma))))
+  (testing "pipe delimited"
+    (is (= test-records
+           (extract-records-from-string test-input-pipe))))
+  (testing "space delimited"
+    (is (= test-records
+           (extract-records-from-string test-input-space)))))
